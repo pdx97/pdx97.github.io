3cca679e625c72640176bf8c02d7f1657dbcca2a
Prakhar Dixit <28250686+pdx97@users.noreply.github.com>
2026-09-08 00:27:42 -0400

    Delete website-renewal/website-renewal directory

--- website-renewal/website-renewal/assets/js/legacy.js
+++
@@ -1,4 +0,0 @@
-(() => {
-  const routes = { intro: 'index.html', work: 'experience.html', project: 'projects.html', education: 'education.html', resume: 'Docs/Prakhar_Dixit_CV.pdf', contact: 'contact.html', publications: 'publications.html' };
-  window.location.replace(routes[window.location.hash.slice(1)] || 'index.html');
-})();
